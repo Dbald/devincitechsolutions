24e1bdfd6c690c5f2f2560d3ce4d51cce5e27549
itsdevinci <devinbaldwin315@gmail.com>
2019-07-13 14:43:01 -0700

    p

--- src/components/navbar/tabbar.js
+++ src/components/navbar/tabbar.js
@@ -8,21 +8,50 @@ class TabBar extends Component {
     return (
       <Router>
         <div id="tabbar" className="header">
-          <div className="main-div">
+          <div className="tab">
             <Link className="links" to="/about">
-            <img className="icon" src="https://piskel-imgstore-b.appspot.com/img/2a468e8f-a5a8-11e9-8683-75ba66f9cf49.gif" alt="about-icon"/>
+              <img
+                className="icon"
+                src="https://piskel-imgstore-b.appspot.com/img/2a468e8f-a5a8-11e9-8683-75ba66f9cf49.gif"
+                alt="about-icon"
+              />
+              <p className="undertext">About</p>
             </Link>
+          </div>
+          <div className="tab">
             <Link className="links" to="/services">
-            <img className="icon" src="https://piskel-imgstore-b.appspot.com/img/0a7cc028-a5ad-11e9-8c18-75ba66f9cf49.gif" alt="service-icon"/>
+              <img
+                className="icon"
+                src="https://piskel-imgstore-b.appspot.com/img/0a7cc028-a5ad-11e9-8c18-75ba66f9cf49.gif"
+                alt="service-icon"
+              />
+              <p className="undertext">Services</p>
             </Link>
+          </div>
+          <div className="tab">
             <Link className="links" to="/home">
               <img className="logo" src={logo} alt="dts" />
+              <p className="undertext">Home</p>
             </Link>
+          </div>
+          <div className="tab">
             <Link className="links" to="/media">
-              Media
+              <img
+                className="icon"
+                src="https://piskel-imgstore-b.appspot.com/img/7bd6bf02-a5b5-11e9-bdce-75ba66f9cf49.gif"
+                alt="media-icon"
+              />
+              <p className="undertext">Media</p>
             </Link>
+          </div>
+          <div className="tab">
             <Link className="links" to="/contact">
-            <img className="icon" src="https://piskel-imgstore-b.appspot.com/img/6ea37e5e-a5a6-11e9-8ab6-75ba66f9cf49.gif" alt="contact-icon"/>
+              <img
+                className="icon"
+                src="https://piskel-imgstore-b.appspot.com/img/6ea37e5e-a5a6-11e9-8ab6-75ba66f9cf49.gif"
+                alt="contact-icon"
+              />
+              <p className="undertext">Contact</p>
             </Link>
           </div>
         </div>
