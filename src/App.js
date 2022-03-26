38ab8d55c59ccb05f0c18b9e8e7d08094db20ac4
Dbald <devinbaldwin315@gmail.com>
2022-03-25 17:25:46 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-// import Header from './components/navbar/header';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 // import Footer from './components/navbar/footer';
 // import TabBar from './components/navbar/tabbar';
 
@@ -16,17 +15,17 @@ class App extends Component {
   render() {
     return (
       <Router>
-      <div className="App">
-        <Route path="/" component={Links}/>
-        {/* <Route exact path="/" component={Home}/>
-        <Route path="/home" component={Home}/>
+        <div className="App">
+          <Route exact path="/" component={Links} />
+          {/* <Route exact path="/" component={Home}/>
+       
         <Route path="/about" component={About}/>
         <Route path="/apps" component={Apps}/>
         <Route path="/home/:services"/>
         <Route path="/contact" component={Contact}/>
         <Route path="/" component={Footer}/>
         <Route path="/" component={TabBar}/> */}
-      </div>
+        </div>
       </Router>
     );
   }
--- src/pages/links/index.js
+++ src/pages/links/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './index.css';
 
 class Links extends Component {
@@ -10,14 +11,14 @@ class Links extends Component {
                 </div>
                 <div className="link_section">
                     <a className="hyper" href="https://calendly.com/itsdevinci/meeting" target="_blank" rel="noopener noreferrer"><button className="link_button">Schedule Time With Me</button></a>
-                    <a className="hyper" href="https://itsdevinci.com/" target="_blank" rel="noopener noreferrer"><button className="link_button">Portfolio</button></a>
+                    <a className="hyper" href="https://drive.google.com/file/d/1QEgBFYfpQKludRks1X9wKQO23YiwlVub/view?usp=sharing" target="_blank" rel="noopener noreferrer"><button className="link_button">Resume</button></a>
                     <a className="hyper" href="https://opensea.io/accounts/itsdevinci" target="_blank" rel="noopener noreferrer"><button className="link_button">NFTs</button></a>
                     <a className="hyper" href="https://forms.gle/QbdmX6wmixnCavHAA" target="_blank" rel="noopener noreferrer"><button className="link_button">XR Interest Form</button></a>
 
                 </div>
                 <div className="icon_holder">
-                    <a id="insta" className="hyper" href="https://www.instagram.com/itsdevincicode/" target="_blank" rel="noopener noreferrer"><img className="icon" src="https://i.imgur.com/aFbzi27.png" alt="gram" /></a>
-                    <a id="yt" className="hyper" href="https://www.youtube.com/user/ThunderKatz100" target="_blank" rel="noopener noreferrer"><img className="icon" src="https://i.imgur.com/eCBuQRQ.png" alt="yt" /></a>
+                    <a id="insta" className="hyper" href="https://www.instagram.com/thedevincicode/" target="_blank" rel="noopener noreferrer"><img className="icon" src="https://i.imgur.com/aFbzi27.png" alt="gram" /></a>
+                    <a id="yt" className="hyper" href="https://www.youtube.com/channel/UCdWMwP8ynch1I0fkgTZUrLw" target="_blank" rel="noopener noreferrer"><img className="icon" src="https://i.imgur.com/eCBuQRQ.png" alt="yt" /></a>
                     <a id="twitch" className="hyper" href="https://www.twitch.tv/thedevincicode" target="_blank" rel="noopener noreferrer"><img className="icon" src="https://i.imgur.com/t868YiR.png" alt="t" /></a>
                 </div>
             </div>
